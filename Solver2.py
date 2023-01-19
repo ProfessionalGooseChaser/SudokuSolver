5f1dacfd6259ea48d9e631680cd663975dc4ad42
Cole Hansen <99274668+ProfessionalGooseChaser@users.noreply.github.com>
2023-01-19 14:13:32 -0500

    Solver2.py v2.05
    I forgot to check my naming nomenclature. But I finally figured out the bug that was bothering me. May be finished soon

--- Solver2.py
+++ Solver2.py
@@ -1,20 +1,23 @@
-from functools import reduce
-import numpy as np
+from functools import reduce    #used this like once
+import numpy as np              #for the matrix math
 
-PUZZLE = [
-    [0, 0, 3, 0, 2, 0, 6, 0, 0],
-    [9, 0, 0, 3, 0, 5, 0, 0, 1],
-    [0, 0, 1, 8, 0, 6, 4, 0, 0],
-    [0, 0, 8, 1, 0, 2, 9, 0, 0],
-    [7, 0, 0, 0, 0, 0, 0, 0, 8],
-    [0, 0, 6, 7, 0, 8, 2, 0, 0],
-    [0, 0, 2, 6, 0, 9, 5, 0, 0],
-    [8, 0, 0, 2, 0, 3, 0, 0, 9],
-    [0, 0, 5, 0, 1, 0, 3, 0, 0] 
-]
-
+#---------------
+#Testing Puzzle
+PUZZLE =(
+    (0, 0, 3, 0, 2, 0, 6, 0, 0),
+    (9, 0, 0, 3, 0, 5, 0, 0, 1),
+    (0, 0, 1, 8, 0, 6, 4, 0, 0),
+    (0, 0, 8, 1, 0, 2, 9, 0, 0),
+    (7, 0, 0, 0, 0, 0, 0, 0, 8),
+    (0, 0, 6, 7, 0, 8, 2, 0, 0),
+    (0, 0, 2, 6, 0, 9, 5, 0, 0),
+    (8, 0, 0, 2, 0, 3, 0, 0, 9),
+    (0, 0, 5, 0, 1, 0, 3, 0, 0) 
+)
+#Puzzle Class
 class Puzz():
     def __init__(self, puzzle):
+        #organizes the puzzle into rows, cols and squares
         self.rows = puzzle
         self.cols = self.find_col()
         self.sqrs = self.find_sqr()
@@ -22,6 +25,7 @@ class Puzz():
     def __str__(self):
         return str(self.rows)
     
+    #finds the columns of the puzzle array
     def find_col(self):
         cols = []
         col = []
@@ -32,8 +36,8 @@ class Puzz():
             col = []
         return cols
 
+    #finds the 3x3 squares in the array
     def find_sqr(self):
-
         Arr = []
         sqr = []
         for i in range(3):
@@ -45,17 +49,24 @@ class Puzz():
                 sqr = []
         return Arr
 
+#This big class is how I'm organizing all the stuff
 class Solution():
     def __init__(self, puzzle):
+        #puzzle Object
         self.puzz = puzzle
+        #matrix A and B, used for solving
         self.MatrixA = []
         self.MatrixB = []
+        #what numbers I need for any given row/col/sqr
         self.constraints = []
+        #fills the matrixes on initialization
         self.fillB()
         self.fillA()
+        #Find what I need for each clue
         self.setConstraints()
     
     def fillB(self):
+        #The sum of 1-9 is 45. So the remain variables must be 45 - sum(i)
         for i in self.puzz.rows:
             self.MatrixB.append(45 - sum(i))
         for j in self.puzz.cols:
@@ -68,10 +79,11 @@ class Solution():
     
     def A_row(self):
         R = []
+        #Organizes puzzle rows into the form factor of the Matrix
         for i in range(len(self.puzz.rows)):
             row = []
             for x in range(len(self.puzz.rows)):
-                for y in range(len(self.puzz.rows[0])):
+                for y in range(len(self.puzz.rows[x])):
                     if (i == x):
                         if (self.puzz.rows[x][y]) == 0:
                             row.append(1)
@@ -83,6 +95,7 @@ class Solution():
     
     def A_col(self):
         C = []
+        #organizes the puzzle columns into the matrix form factor
         for i in range(len(self.puzz.rows[0])):
             col = []
             for x in range(len(self.puzz.rows)):
@@ -99,11 +112,12 @@ class Solution():
     def A_sqr(self):
         #hard coded for a 9x9 puzzle sorry
         S = []
+        #organizes the sqrs into the matrix form factor
         for i in range(3):
             for j in range(3):
                 sqr = []
                 for x in range(len(self.puzz.rows)):
-                    for y in range(len(self.puzz.rows[0])):
+                    for y in range(len(self.puzz.rows[x])):
                         if (i == x//3 and j == y//3):
                             if (self.puzz.rows[x][y]) == 0:
                                 sqr.append(1)
@@ -122,12 +136,15 @@ class Solution():
             self.MatrixA.append(s)
         for i in self.MatrixA:
             if sum(i) == 0:
-                print(i)
-                self.MatrixA.remove(i)
-
+                self.MatrixA.remove(i) #This is doing something it shouldn't. I think
+        
     def setConstraints(self):
+
+        #Can I use quicksort(i) - set(range(1, 10)) to get rid of the for loop?
+
         #Row Constraints
         Rc = []
+        #what does each row need?
         for i in self.puzz.rows:
             temp = list(range(1, 10))
             for j in i:
@@ -138,6 +155,7 @@ class Solution():
 
         #Column Constraints
         Cc = []
+        #What does each row need?
         for i in self.puzz.cols:
             temp = list(range(1, 10))
             for j in i:
@@ -148,6 +166,7 @@ class Solution():
 
         #Square Constraints
         Sc = []
+        #what does each square need
         for i in self.puzz.sqrs:
             temp = list(range(1, 10))
             for j in i:
@@ -157,49 +176,59 @@ class Solution():
         self.constraints.append(Sc)
 
     def Solvable(self):
+        #print(np.matrix(self.MatrixA))
         if len(self.MatrixA[0]) != len(self.MatrixB):
+            #Checks to see if the matrixes have matching sizes
             return False
         elif np.linalg.det(np.array(self.MatrixA)) == 0:
-            print(self.MatrixA)
+            #is the determinant non-zero?
             return False
         else:
+            #looks like we're good
             return True
 
     def Solve(self):
+        #just solving the matrix
+        #this way I don't have to guess the last 24ish vairables
         A = np.array(self.MatrixA)
         B = np.array(self.MatrixB)
-        for i in A:
-            print(i)
-            print('\n')
+        
         return np.linalg.solve(A, B)
 
 class branch():
+    #created a quick tree-like data structure. 
     def __init__(self, parent, sol):
         self.parent = parent
         self.data = sol
         self.children = []
 
 def guess(given):
+    #this is fucked up. It changes thingys it shouldnt. I need to create a global variable
     puzpuz = given.data.puzz.rows
     possibilities = []
     for x in range(len(puzpuz)):
         for y in range(len(puzpuz[0])):
-            
             if puzpuz[x][y] == 0:
                 intersects = reduce(np.intersect1d, (given.data.constraints[0][x], given.data.constraints[1][y], given.data.constraints[2][(3 * (x//3)) + (y//3)]))
                 for p in intersects:
-                    puzpuz[x][y] = p
-                    temp = branch(given, Solution(Puzz(puzpuz)))
+                    tmpPuz = list(puzpuz)
+                    tmpPuz[x] = list(tmpPuz[x])
+                    tmpPuz[x][y] = p
+                    temp = branch(given, Solution(Puzz(tmpPuz)))
                     possibilities.append(temp)
+                    tmpPuz[x] = tuple(tmpPuz[x])
     return possibilities
 
+
+
 def iter_improv(initial):
+    #systematically takes guesses
     while True:
         possibilities = guess(initial)
-        if initial.data.puzz.rows in solset:
+        if initial in solset:
             break
         else:
-            solset.add(initial.data.puzz.rows)
+            solset.add(initial)
         if possibilities == []:
             #this branch doesn't lead anywhere, therefore it also cannot be solvable
             print("dead branch")
@@ -208,10 +237,30 @@ def iter_improv(initial):
             for p in possibilities:
                 #checking to see if it's solvable
                 if p.data.Solvable():
+                    print(p.Solve())
                     return p.Solve()
                 else:
                     iter_improv(p)
 
 solset = set()
 
-iter_improv(branch(None, Solution(Puzz(PUZZLE))))
+
+
+#Testing
+#--------------
+
+tester = branch(None, Solution(Puzz(PUZZLE))) #same as the original
+
+pp = tester.data.puzz.rows
+
+for p in pp:
+    print(p, sum(p), 45 - sum(p))
+
+print('-------------------------------')
+
+pp2 = guess(tester)
+for i in pp2:
+    print(np.matrix(i.data.puzz.rows))
+
+
+
