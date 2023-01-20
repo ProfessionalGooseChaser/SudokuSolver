26e43a4d6066c69978b5c8cc145ab18f865faf79
Cole Hansen <99274668+ProfessionalGooseChaser@users.noreply.github.com>
2023-01-20 11:58:02 -0500

    SudokerReader.py v1.02
    It's mostly working. It has an issue with some fonts, It reads 5s as 3s which is annoying.

--- SudokuReader.py
+++ SudokuReader.py
@@ -6,6 +6,8 @@
 
 from PIL import Image
 from pytesseract import pytesseract
+import numpy as np
+import time
 
 pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
 src1 = r"ExamplePuzzle1.png"
@@ -25,63 +27,54 @@ def find_imgs(src):
     for i in range(9):      #y
         for j in range(9):  #x
             temp = img.crop((j*SqrLength + 8, i*SqrHeight + 8, (j+1)*SqrLength - 8, (i+1)*SqrHeight - 8))
-            imgs.append(temp.resize((700, 700))) #scaling up the image so it's easier to read
+            imgs.append(temp.resize((900, 900))) #scaling up the image so it's easier to read
     return imgs
 
 
 def create_puzzle(imgs):
     Arr = []
     row = []
-    for img in imgs:
+    for i, img in enumerate(imgs):
         txt = pytesseract.image_to_string(img, lang="eng", config="--psm 10")
-        if txt not in nums:
-            row.append("")
-        else:
-            row.append(txt)
+        #psm10 assumes single char
+        #if i in problems:
+            #img.show()
+        row.append(txt)
         if(len(row) >= 9):
             Arr.append(row)
             row = []
     return Arr
 
+def cleanUp(trix):
+    for x in range(len(trix)):
+        for y in range(len(trix[x])):
+            j = trix[x][y]
+            j = j[0]
+            match j:
+                case '/':
+                    j = 7
+                case 'O':
+                    j = 8
+                case 'i':
+                    j = 1
+                case '°':
+                    j = 8
+                case 'A':
+                    j = 4
+                case '>':
+                    j = 5
+                case '_':
+                    j = 0
+            j = int(j)
+            trix[x][y] = j
+    return trix
+
 src2 = r"431.webp"
 
-SqrLength, SqrHeight = dimensions(src2)
+SqrLength, SqrHeight = dimensions(src1)
 
-for i in create_puzzle(find_imgs(src2)):
-    print(i)
+start = time.time()
+trix1 = create_puzzle(find_imgs(src1))
 
-#           Puzzle
-#   5|3|0   0|7|0   0|0|0
-#   6|0|0   1|9|5   0|0|0
-#   0|9|8   0|0|0   0|6|0
-
-#   8|0|0   0|6|0   0|0|3
-#   4|0|0   8|0|3   0|0|1
-#   7|0|0   0|2|0   0|0|6
-
-#   0|6|0   0|0|0   2|8|0
-#   0|0|0   4|1|9   0|0|5
-#   0|0|0   0|8|0   0|7|9
-
-#   Output (8)
-#['5\n', '3\n', '', '', '', '', '', '', '']             missing the 7
-#['6\n', '', '', '1\n', '9\n', '5\n', '', '', '']       All good
-#['', '9\n', '', '', '', '', '', '6\n', '']             missing the 8
-#['', '', '', '', '6\n', '', '', '', '3\n']             missing the 8
-#['', '', '', '', '', '3\n', '', '', '']                missing 4, 8 and 1 (see if the box is moving too far up)
-#['', '', '', '', '2\n', '', '', '', '6\n']             missing the 7
-#['', '6\n', '', '', '', '', '2\n', '', '']             missing the 8
-#['', '', '', '', '', '9\n', '', '', '']                mssing 4, 1, 5
-#['', '', '', '', '', '', '', '', '9\n']                missing the 8 and 7
-
-#output (9)                                             Missing:
-#['5\n', '3\n', '', '', '', '', '', '', '']             7
-#['6\n', '', '', '1\n', '9\n', '5\n', '', '', '']       None
-#['', '9\n', '6\n', '', '', '', '', '6\n', '']          Read the 8 as the 6???
-#['6\n', '', '', '', '6\n', '', '', '', '3\n']          Read the 8 as a 6. 
-#['', '', '', '', '', '3\n', '', '', '1\n']             4, 8
-#['', '', '', '', '2\n', '', '', '', '6\n']             7
-#['', '6\n', '', '', '', '', '2\n', '', '']             8
-#['', '', '', '', '', '', '', '', '3\n']                Missed 4, 1, 9 and read the 5 as a 9
-#['', '', '', '', '', '', '', '', '9\n']                8, 7
-
+print(np.matrix(cleanUp(trix1)))
+print(time.time() - start)
